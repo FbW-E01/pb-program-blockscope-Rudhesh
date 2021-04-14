f9b6deee79024f6b5d4f4b9423e399481797e8e1
dci <rudeshdhote@gmail.com>
2021-04-14 22:38:02 +0200

    initial commit

---
+++ block.js
@@ -0,0 +1,42 @@
+
+const  determiner = 2 ;
+
+
+if (determiner < 0) {
+    let x = "Less than 0"
+    console.log(x)
+    
+}
+
+else if (determiner > 0) {
+    let x = "Greater or equal to 0"
+    console.log(x)
+    
+}
+
+
+
+// New Variables
+
+
+
+if (determiner >= 0) {
+    let updater = "Greater or equal to 0"
+    console.log(updater)
+
+    let message = "Positive Integer" 
+
+    console.log(message);
+    
+}
+
+
+
+if (determiner < 0) {
+
+    let updater = "Less than 0"
+    
+    console.log(updater)
+}
+
+// if we try to print message outside it will not defined because curly bracket block the statement
